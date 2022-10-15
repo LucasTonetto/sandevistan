d580fcf9e14ad7338aaeb88b15f36e150c7e5604
Lucas Tonetto <lucas.firmo@dp6.com.br>
2022-10-14 22:25:56 -0300

    feat(SandevistanSrc): :sparkles: Adicao de politicas de ciclo de vida em bucket s3

--- sandevistan/components/S3Bucket.py
+++ sandevistan/components/S3Bucket.py
@@ -5,10 +5,18 @@ class S3Bucket(s3_bucket.S3Bucket):
 
     def __init__(self, scope: Construct, bucket_id: str, **kwargs):
 
-        tags = {"cdktf-sandevistan": scope.name}
-
         if("tags" in kwargs):
-            tags = {**tags, **kwargs["tags"]}
-            del kwargs["tags"]
+            kwargs["tags"] = {"cdktf-sandevistan": scope.name, **kwargs["tags"]}
+        else:
+            kwargs["tags"] = {"cdktf-sandevistan": scope.name}
 
-        super().__init__(scope, bucket_id, tags=tags, **kwargs)
+        if("lifecycle_rule" not in kwargs):
+            kwargs["lifecycle_rule"] = [
+                { "enabled": True, "id": "abort-multipart", "prefix": "/", "abortIncompleteMultipartUploadDays": 7 },
+                { "enabled": True, "id": "standard-ia-files-30-days", "transition": [{ "days": 30, "storageClass": "STANDARD_IA" }] },
+                { "enabled": True, "id": "standard-ia-files-noncurrent-version-30-days", "noncurrentVersionTransition": [{ "days": 30, "storageClass": "STANDARD_IA" }] },
+                { "enabled": True, "id": "glacier-flexible-retrieval-files-180-days", "transition": [{ "days": 180, "storageClass": "GLACIER" }] },
+                { "enabled": True, "id": "glacier-flexible-retrieval-files-noncurrent-version-180-days", "noncurrentVersionTransition": [{ "days": 180, "storageClass": "GLACIER" }] },
+            ]
+
+        super().__init__(scope, bucket_id, **kwargs)

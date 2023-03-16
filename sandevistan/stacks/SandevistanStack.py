f1a1294b170070c440e5e5df65ad61a88fc5ff29
Lucas Tonetto Firmo <lucasfirmo@hotmail.com>
2023-03-16 18:04:14 -0300

    fix: :bug: Correcao do SandevistanStack definindo o path de exportacao

--- sandevistan/stacks/SandevistanStack.py
+++ sandevistan/stacks/SandevistanStack.py
@@ -1,8 +1,11 @@
 from cdktf import TerraformStack
 from constructs import Construct
+import sys
 
 class SandevistanStack(TerraformStack):
     def __init__(self, scope: Construct, name: str):
         super().__init__(scope, name)
 
         self.name = name
+
+        sys.path.insert(0,'../..')
